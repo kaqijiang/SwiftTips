072d197ae89f38785c518ed16375022c6da6a22f
侯伟杰 <kaqijiang@sina.cn>
2018-05-23 10:56:14 +0800

    6gif

---
+++ [7]LongitudeLatitude/[7]LongitudeLatitude/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  [7]LongitudeLatitude
+//
+//  Created by Seven on 2018/5/22.
+//  Copyright © 2018年 侯伟杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
